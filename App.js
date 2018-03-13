a08564b757c7398c648f597365b142ea6447b31d
devisscher <devisscher.thomas@gmail.com>
2018-03-13 07:40:16 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Camera from './Camera';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'red'}}>Welcome to snaphole</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Camera />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Camera.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+import {
+  Permissions,
+  BarCodeScanner
+} from 'expo';
+
+export default class Camera extends React.Component {
+
+  state = {
+    hasCameraPermission: false
+  }
+
+  async componentWillMount() {
+    const permissions = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: permissions.status === 'granted'
+    });
+  }
+
+  render() {
+    if (this.state.hasCameraPermission) {
+      return (
+        <BarCodeScanner
+          type={this.props.type || 'front'}
+          key={`front-${this.props.type}`}
+          style={styles.camera}
+        />
+      )
+    }
+
+    return <View style={styles.placeholder} />
+  }
+}
+
+const styles = StyleSheet.create({
+  placeholder: {
+    flex: 1,
+    backgroundColor: '#eee'
+  },
+  camera: {
+    flex: 1
+  }
+})
